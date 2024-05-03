7dab1827003283f9b0b10c779133c8273c94b5d7
Manoj-kumar-C <developermanoj2003@gmail.com>
2024-05-03 22:34:12 +0530

    update

---
+++ others/hashing.py
@@ -0,0 +1,15 @@
+# Hashing a string
+#sha256 algo
+my_string = "Hello, World!"
+hashed_string = hash(my_string)
+print("Hashed string:", hashed_string)
+
+# Hashing an integer
+my_integer = 1.5
+hashed_integer = hash(my_integer)
+print("Hashed integer:", hashed_integer)
+
+# Hashing a tuple
+my_tuple = (1, 2, 3) # 
+hashed_tuple = hash(my_tuple)
+print("Hashed tuple:", hashed_tuple)
--- stack.py
+++ stack.py
@@ -4,13 +4,16 @@ class Stack:
         
     def push(self, item):
         self.items.append(item)
+        #items[i]= item
     def pop(self):
         self.items.pop()
+        
     def isEmpty(self):
         self.items.is_empty()
         
 stack = Stack()
 stack.push(20)
-
+stack.push(50)
+stack.pop()
 stack.push(30)
 print(stack.items)
